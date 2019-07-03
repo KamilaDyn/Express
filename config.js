b18853ea8bcecc11412d679cab3c1c9cee81e4da
Kamila Dynysiuk <kamila0990@gmail.com>
2019-07-03 20:03:21 +0100

    config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   db: `mongodb+srv://admin:123@cluster0-ql9v3.mongodb.net/test?retryWrites=true&w=majority`,
-  keySession: ["Twoj_klucz"],
+  keySession: ["TWOJKLUCZ"],
   maxAgeSession: 24 * 60 * 60 * 1000
 };
